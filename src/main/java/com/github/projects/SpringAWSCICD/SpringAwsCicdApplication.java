c59621f8f1a214f33c068b4b439bacb5629e9835
Kurella Yash <59912430+yash-k9@users.noreply.github.com>
2020-12-19 15:17:15 +0530

    Reconfiguring Files

---
+++ src/main/java/com/github/projects/SpringAWSCICD/SpringAwsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.github.projects.SpringAWSCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/projects/SpringAWSCICD/SpringAwsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projects.SpringAWSCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
